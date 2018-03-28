106f186186ea2d8a37b5d1daac6c009c7d09d626
ziran24 <37875894+ziran24@users.noreply.github.com>
2018-03-28 22:29:33 +0700

    Add files via upload

---
+++ Keep.js
@@ -0,0 +1,10 @@
+var nama;
+ nama=prompt('Boleh tahu siapa nama anda?');
+ if(nama){
+  alert('Apa kabar '+nama+'! Saya Zi_Ran2425, Masih ingatkah denganku?');
+ }else{
+  alert('Hhmm sepertinya anda lupa siapa saya.');
+ }else{
+var nama;
+ nama=prompt('Coba Tebak Siapa Saya?');
+ if(nama){
