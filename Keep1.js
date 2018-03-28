7258b7e5da707805a83b740a78370f683ca75af9
ziran24 <37875894+ziran24@users.noreply.github.com>
2018-03-28 22:47:42 +0700

    Add files via upload

---
+++ Keep1.js
@@ -0,0 +1,7 @@
+var nama;
+ nama=prompt('Boleh tahu siapa nama anda?');
+ if(nama){
+  alert('Apa kabar '+nama+'! Saya Zi_Ran2425, Masih ingatkah denganku?');
+ }else{
+  alert('Hhmm sepertinya anda lupa siapa saya.');
+ }
