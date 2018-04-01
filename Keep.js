83a871cc89b127ae69b9eee86cceec2ad60f255e
Ziran <37875894+ziran24@users.noreply.github.com>
2018-04-01 22:16:52 +0700

    Update Keep.js

--- Keep.js
+++ Keep.js
@@ -3,5 +3,8 @@ var nama;
  if(nama){
   alert('Apa kabar '+nama+'! Saya Zi_Ran2425, Masih ingatkah denganku?');
  }else{
-  alert('Hhmm sepertinya anda lupa siapa saya.');
- }
+var nama;
+ nama=prompt('Boleh tahu siapa nama anda?');
+ if(nama){
+  alert('Apa kabar '+nama+'! Saya Zi_Ran2425, Masih ingatkah denganku?');
+ }else{
