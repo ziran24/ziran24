2c9eb05a081be996a8e78d6cf5724f6fdc4e8f32
Ziran <37875894+ziran24@users.noreply.github.com>
2018-04-03 22:09:04 +0700

    Update Keep.js

--- Keep.js
+++ Keep.js
@@ -1,16 +1,19 @@
 var nama;
- nama=prompt('Boleh tahu siapa nama anda?');
+ nama1=prompt('Boleh tahu siapa nama anda?');
+<input type="submit" value="Yakin">
  if(nama){
-  alert('Benarkah ini '+nama+'?');
+  alert('Benarkah ini '+nama1+'?');
  }
  if(nama){
-  alert('Apa kabar '+nama+'! Saya Zi_Ran2425, Masih ingatkah denganku?');
+  alert('Apa kabar '+nama1+'! Saya Zi_Ran2425, Masih ingatkah denganku?');
  }
  if(nama){
-  alert('Hhmm sepertinya '+nama+' lupa siapa saya.');
+  alert('Hhmm sepertinya '+nama1+' lupa siapa saya.');
  }
 var nama;
- nama=prompt('Tebak siapa saya?');
+ nama2=prompt('Tebak siapa saya?');
+<input type="submit" value="Yakin">
  if(nama){
-  alert('Yakin nama saya '+nama+'?');
+  alert('Yakin '+nama1+' nama saya '+nama2+'?');
  }
+<input type="submit" value="Yakin">
